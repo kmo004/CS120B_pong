392f9dba408236080b9235804b86cefd1efcdda7
kmo004 <kmo004@ucr.edu>
2019-07-18 16:55:05 -0700

    deleted double name

--- source/main.c
+++ source/main.c
@@ -7,15 +7,6 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-/*	Author: kmo004
- *  Partner(s) Name: Michael Wen
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
 #include <avr/io.h>
 #include <io.h>
 #include <avr/interrupt.h>
